159b119b9f473a53cecc94467184a2cf01e3a373
shashwatsingh <shashwatsingh005@gmail.com>
2022-10-14 01:07:13 +0530

    App file update

--- cypress/e2e/console/app.cy.js
+++ cypress/e2e/console/app.cy.js
@@ -1,33 +1,34 @@
 ///  <referencec types = "cypress" />
 
+describe('Create app',function(){
 
 
-describe('home functionality',function(){
-
-    it('login', function()  {
-    
+    it('Create App', ()=>   {
         cy.visit('https://auth.kloudlite.io')
         cy.contains('Login With Email').click()
-           cy.get('.gap-3 > :nth-child(1) > .flex > .w-full').type('shashwat@plaxonic.com')
-           cy.get('.gap-3 > :nth-child(2) > .flex').type('1234567890')
-           cy.contains('Sign in').click()
-           cy.wait(1000)
-           cy.url().should('include','/home')
+        cy.get('.gap-3 > :nth-child(1) > .flex > .w-full').type('shashwat@plaxonic.com')
+        cy.get('.gap-3 > :nth-child(2) > .flex').type('1234567890')
+        cy.contains('Sign in').click()
+        cy.wait(1000)
+        cy.url().should('include','/home')
+        cy.wait(1000)
 
+        cy.get('.overflow-hidden > .flex-1').focus().type('single-67714')                     //Search Project
+        cy.get('.gap-4 > .flex-col > .font-medium').click()
+        cy.url().should('include','https://console.kloudlite.io/apps')
+        cy.contains('single').should('be.visible')
+        
+         cy.contains('Create New App').click()
+         cy.get('.pb-3 > :nth-child(1) > .flex > .w-full').type('App1')                            // APP NAME
+         cy.get('.pb-3 > :nth-child(2) > .flex > .w-full').type('App1-123')                         //App ID
+         cy.get('.pb-3 > :nth-child(3) > .flex > .w-full').type('Description')
+         cy.get('.py-6 > .transition-colors').click()                                                 // Let's get started button
 
-           //cy.get('.overflow-hidden > .flex-1').type('xyz').click()
-          // cy.get('.gap-4 > .flex-col > .font-semibold').click()
-           //cy.get(".font-semibold text-base leading-6 text-neutral-800 capitalize").select('Xyz').click()
-cy.get('.flex flex-col leading-normal').select('Xyz').click()
+         
 
-
-
-cy.get('[href="/apps/?projectId=proj-hinviqepcek1k1stojnu5pg1-z0w&accountId=acc-49cslf3qe3bi-fcqyg7shwrnuvqq"] > .bg-white > .gap-4')
-
-cy.get('[href="/apps/?projectId=proj-9rupx57f2lawxzf8izunydxhd8gw&accountId=acc-49cslf3qe3bi-fcqyg7shwrnuvqq"] > .bg-white > .gap-4')
 
 
 
     })
-
+    
 })
